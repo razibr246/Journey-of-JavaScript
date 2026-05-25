c4522454e97b368cd3b81e832a6c1e179a4bceca
Razib Miah <razibr246@gmail.com>
2026-05-25 22:58:19 +0600

    Add sentence maker with two story examples

---
+++ Working on workshops/Build a Sentence Maker.js
@@ -0,0 +1,21 @@
+let adjective = "big";
+let noun = "dragon";
+let verb = "running";
+let place = "castle";
+let adjective2 = "smoky";
+let noun2 = "pizza";
+
+let firstStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+
+console.log(`First story: ${firstStory}`);
+
+adjective = "tiny";
+noun = "rabbit";
+verb = "jumping";
+place = "forest";
+adjective2 = "shiny";
+noun2 = "carrots";
+
+let secondStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+
+console.log(`Second story: ${secondStory}`);
